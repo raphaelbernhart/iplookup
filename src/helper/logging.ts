59ee3e06255e7c556edf5bb24139501e7f39d090
Raphael Bernhart <mail@raphaelbernhart.at>
2020-08-21 17:48:33 +0200

    added Logger Helper

--- src/helper/logging.ts
+++ src/helper/logging.ts
@@ -1,13 +1,13 @@
-const prefix = "\x1b[36m[IP]\x1b[0m";
+const prefix = "\x1b[36m[IP]\x1b[0m ";
 const reset = "\x1b[0m";
 
 export default class Logger {
     static info(InfoMessage: string): void {
-        console.error(prefix + "\x1b[37m Info:    " + InfoMessage + reset);
+        console.error(prefix + "\x1b[37m" + InfoMessage + reset);
     }
 
     static success(SuccessMessage: string): void {
-        console.error(prefix + "\x1b[32m Success: " + SuccessMessage + reset);
+        console.error(prefix + "\x1b[32m" + SuccessMessage + reset);
     }
 
     static error(ErrorMessage: string|Error): void {
